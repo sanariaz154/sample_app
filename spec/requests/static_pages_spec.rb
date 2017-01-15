e51cbb0c5b0140ab7f570223ad4095f5d82bf9ce
sanariaz154 <sanariaz154@gmail.com>
2017-01-16 02:39:32 +0500

    modified rails_helper file modified static_pages_spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,46 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe "Static pages" do
-   
-    describe "Home page" do
-      it "should have the content 'Sample App'" do
-        visit '/static_pages/home'
-        page.should have_content('Sample App')
-      end
+RSpec.describe "Static Pages", type: :request do
+  describe "Home page" do
+    it "should have content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
     end
+  end
+
+
+describe "About page" do
+    it "should have content 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+     # page.should have_selector('title', :text => 'About')
+    end
+
+  end
+
+describe "Help page" do
+it "should have the content 'Help'" do
+visit '/static_pages/help'
+page.should have_content('Help')
+end
+end
+
+describe "contact page" do
+it "should have the content 'contact'" do
+visit '/static_pages/contact'
+page.should have_content('contact')
+end
+end
+
+
+
+
+
+
+
+
+
+
+
+
 end
